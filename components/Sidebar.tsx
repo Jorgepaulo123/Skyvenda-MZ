e18f42db6f367730b88808d36e4c42f2e69c36aa
BlueSpark MZ <bluesparkmz@users.noreply.github.com>
2025-11-13 16:45:08 -0200

    migrado para nextjs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import { LayoutDashboard } from 'lucide-react';
 
 export function Sidebar() {
@@ -9,7 +9,7 @@ export function Sidebar() {
           <h2 className="mb-2 px-4 text-lg font-semibold">Menu</h2>
           <div className="space-y-1">
             <Link
-              to="/overview"
+              href="/overview"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
             >
               <LayoutDashboard className="h-4 w-4" />
@@ -21,4 +21,4 @@ export function Sidebar() {
       </div>
     </div>
   );
-} 
+}
